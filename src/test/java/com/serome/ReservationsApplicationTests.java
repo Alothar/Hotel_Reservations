03e5000a2846bdd02ec2d13330339593d5e72ec4
Sergey Omelchuk <sergomel52@gmail.com>
2020-02-09 19:34:34 +0100

    Initial commit

---
+++ src/test/java/com/serome/ReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
